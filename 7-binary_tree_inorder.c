ade6b690dda03c4a21ef4fcbe62331540c2c31e8
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-05 01:05:58 +0200

    MM

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - print the tree in in-oreder traversal
+ * @tree: is a pointer to the root of the tree
+ * @func: is a pointer function
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	(*func)(tree->n);
+	binary_tree_preorder(tree->right, func);
+}
