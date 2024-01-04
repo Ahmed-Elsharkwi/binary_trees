449bee64ef11072008c55af971c9178af3019317
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-05 01:34:50 +0200

    MM

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - print the tree in in-oreder traversal
+ * @tree: is a pointer to the root of the tree
+ * Return: 0 if tree is NULL or 1 if not
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	flag = 0;
+	count = 0;
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+	{
+		count = 1 + binary_tree_height(tree->left);
+		flag = 1
+	}
+	if (tree->right && flag == 0)
+		count = 1 + binary_tree_height(tree->right);
+	return (count);
+}
