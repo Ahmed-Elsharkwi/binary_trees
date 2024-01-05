5ff2611d6142b6341c7149b72b591f33158887c1
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-06 01:26:36 +0200

    MM

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - calculate the number of the leaves with a least
+ * one child
+ * @tree: is a pointer to the root of the tree
+ * Return: 0 if tree is NULL or number of leaves if not
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int count_1 = 0, count_2 = 0, count = 0;
+
+	if (tree == NULL)
+
+		return (0);
+
+	if (tree->left)
+	{
+		count_1 = binary_tree_height(tree->left) + 1;
+	}
+	if (tree->right)
+
+		count_2 = binary_tree_height(tree->right) + 1;
+	count = count_1 - count_2;
+
+	return (count);
+}
+/**
+ * binary_tree_height - print the tree in in-oreder traversal
+ * @tree: is a pointer to the root of the tree
+ * Return: 0 if tree is NULL or 1 if not
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t count_1 = 0, count_2 = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+	{
+		count_1 = 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right)
+		count_2 = 1 + binary_tree_height(tree->right);
+	if (count_1 >= count_2)
+		return (count_1);
+	else
+		return (count_2);
+}
